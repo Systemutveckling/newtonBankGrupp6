21bee41c85d2c2f17964811d77beea53ba7d8b33
hampus <hampus@10.11.12.206>
2016-10-25 08:39:56 +0200

    Added more methods to banklogic

--- Newtonbank/testNewtonBank/src/testnewtonbank/BankLogic.java
+++ Newtonbank/testNewtonBank/src/testnewtonbank/BankLogic.java
@@ -13,12 +13,11 @@ import java.util.List;
  * @author skate
  */
 public class BankLogic {
-    
-    
-    private ArrayList <Customer> customerList = new ArrayList();
-    
-    public BankLogic(){
-        
+
+    private ArrayList<Customer> customerList = new ArrayList();
+
+    public BankLogic() {
+
     }
 
     public ArrayList<Customer> getCustomerList() {
@@ -28,23 +27,58 @@ public class BankLogic {
     public void setCustomerList(ArrayList<Customer> customerList) {
         this.customerList = customerList;
     }
-    
-    public boolean addCustomer(String name,Long ssn){
+
+    public boolean addCustomer(String name, Long ssn) {
         Customer c = new Customer(name, ssn);
         customerList.add(c);
-        
+
         return true;
-        
+
     }
-    
-    public List<String> getCostumer(long ssn){
+
+    public List<String> getCostumer(long ssn) {
         List<String> cust = new ArrayList<>();
-        for(Customer c : customerList){
-        if(c.getSsn() == ssn){
-         cust.add(c.getName() + " " + c.getSsn());
+        for (Customer c : customerList) {
+            if (c.getSsn() == ssn) {
+                cust.add(c.getName() + " " + c.getSsn());
+                for(SavingsAccount sa : c.getNumberOfAccount()){
+                    cust.add(sa.getAccountNo() + " " + sa.getAccountType() + " " + sa.getBalance());
+                }
+            }
         }
-        }
-        
+
         return cust;
     }
+
+    public List<String> removeCustomer(long ssn) {
+        List<String> rm = new ArrayList();
+        for (Customer c : customerList) {
+            if (c.getSsn() == ssn) {
+                rm.add("We removed " + c.getName() + " ssn = " + c.getSsn());
+                for (SavingsAccount sa : c.getNumberOfAccount()) {
+                    rm.add(sa.getAccountNo() + " " + sa.getAccountType() + " " + sa.getBalance() + " " + sa.getInterest());
+
+                }
+                customerList.remove(c);
+            }
+
+        }
+        return rm;
+
+    }
+
+    public boolean changeCustomerName(String name, long ssn) {
+
+        for (Customer c : customerList) {
+            if (c.getSsn() == ssn) {
+                c.setName(name);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
 }
